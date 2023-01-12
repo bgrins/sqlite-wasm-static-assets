6143de402755b1b8e128bbb7cef92280cc2df745
Brian Grinstead <briangrinstead@gmail.com>
2023-01-12 13:21:37 -0800

    vendoring in 3400000

---
+++ README.md
@@ -0,0 +1,10 @@
+Vendoring in a new version into a branch, which can then be configured to host on gh-pages:
+
+```
+./vendor.sh 3400000
+```
+
+Previewing locally:
+```
+python3 -m http.server
+```
---
+++ vendor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+ echo "no arguments - pass a version number like 3400000 from https://sqlite.org/download.html"
+ exit 1
+fi
+
+echo "Version: $1";
+
+git checkout --orphan $1
+curl -o sqlite-wasm-$1.zip "https://sqlite.org/2022/sqlite-wasm-$1.zip"
+unzip sqlite-wasm-$1.zip
+mv sqlite-wasm-$1/* .
+rm -r sqlite-wasm-$1 sqlite-wasm-$1.zip
+echo $1 > version.txt
+git status
+git add -A
+git commit -m "vendoring in $1"
